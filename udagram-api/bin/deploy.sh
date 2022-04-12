a97169d9a4f047ccb135ddadde9e23f70bcdad1a
Mohamed Nagy <moh2med.nagy@outlook.com>
2022-04-12 20:59:48 +0200

    first commiit

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,12 +1,12 @@
-# eb setenv POSTGRES_USERNAME= postgres &&
-# POSTGRES_PASSWORD= postgres2022 &&
-# eb setenv POSTGRES_HOST= http://database-1.cwsqc4chitsu.us-west-2.rds.amazonaws.com &&
-# eb setenv POSTGRES_DB= postgres &&
-# eb setenv POSTGRES_PORT= 5432 &&
-# eb setenv PORT= 3000 &&
-# eb setenv AWS_REGION= us-west-2 &&
-# eb setenv AWS_PROFILE= default &&
-# eb setenv AWS_BUCKET= mo-nagy-udagram &&
-# eb setenv URL= http://udagram-api-dev.eba-gntau6iq.us-west-2.elasticbeanstalk.com &&
-# eb setenv JWT_SECRET= jwt-secret &&
+eb setenv POSTGRES_USERNAME= postgres &&
+eb setenv POSTGRES_PASSWORD= postgres2022 &&
+eb setenv POSTGRES_HOST= http://database-1.cwsqc4chitsu.us-west-2.rds.amazonaws.com &&
+eb setenv POSTGRES_DB= postgres &&
+eb setenv POSTGRES_PORT= 5432 &&
+eb setenv PORT= 3000 &&
+eb setenv AWS_REGION= us-west-2 &&
+eb setenv AWS_PROFILE= default &&
+eb setenv AWS_BUCKET= mo-nagy-udagram &&
+eb setenv URL= http://udagram-api-dev.eba-gntau6iq.us-west-2.elasticbeanstalk.com &&
+eb setenv JWT_SECRET= jwt-secret &&
 eb deploy udagram-api-dev
